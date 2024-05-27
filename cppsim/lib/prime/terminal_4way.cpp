21dbbe9067ef66ab9457ed3d2b83830870d72194
BunnySpectrum <bunnyspectrum@gmail.com>
2024-05-26 22:03:07 -0700

    moved 2T into 4T as input/output members

--- cppsim/lib/prime/terminal_4way.cpp
+++ cppsim/lib/prime/terminal_4way.cpp
@@ -1,6 +1,6 @@
 #include "terminal_4way.h"
 
-Terminal_4Way::Terminal_4Way(float v)
- : value(v) {
-    //value = v;
+Terminal_4Way::Terminal_4Way()
+ : input(Terminal_2Way(0)), 
+   output(Terminal_2Way(1)) {
 }
--- cppsim/lib/prime/terminal_4way.h
+++ cppsim/lib/prime/terminal_4way.h
@@ -1,14 +1,16 @@
 #ifndef _TERMINAL_4WAY_H_
 #define _TERMINAL_4WAY_H_
 
+#include "prime/terminal_2way.h"
+
 class Terminal_4Way{
     public:
         int id;
         float value;
-        Terminal_4Way(float v);
-        float voltage();
-        float current();
-        float impedance();
+        Terminal_2Way input;
+        Terminal_2Way output;
+        Terminal_4Way();
+
 };
 
 #endif
