1c9289fd8835551db5ee1119cfcd3de4b04837e4
BunnySpectrum <bunnyspectrum@gmail.com>
2024-06-04 19:16:10 -0700

    python now prints connections per node; started adding rest of power nets to pb

--- cppsim/main/host/main.cpp
+++ cppsim/main/host/main.cpp
@@ -8,7 +8,18 @@
 #include "prime/terminal_2way.h"
 #include "netlist.pb.h"
 
+/*
 
+
+                    +---|  Z3  |-------------------------------+
+                    |                                          |
+                    |                      ________________    |
+(Vin) ---|  Z1  |---+---|  Z2  |---+------|> U1.P          |   |
+                                   |      |        U1.Out  >---+---(Vout)  
+                                   Z4  +--|> U1.N          |   |
+                                   |   |   ----------------    |
+                                 (Gnd) +-----------------------+
+*/
 
 int main(){
     std::cout << "Hello world.\n";
@@ -19,15 +30,26 @@ int main(){
     auto pZ4 = std::make_shared<Terminal_2Way>("Z4");
 
     auto pVin = std::make_shared<Terminal_2Way>("Vin");
+    auto pVdd = std::make_shared<Terminal_2Way>("Vdd");
+    // auto pGnd = std::make_shared<Terminal_2Way>("Gnd");
 
     auto pU1Out = std::make_shared<Terminal_2Way>("U1.Out");
     auto pU1N = std::make_shared<Terminal_2Way>("U1.N");
     auto pU1P = std::make_shared<Terminal_2Way>("U1.P");
+    auto pU1Vdd = std::make_shared<Terminal_2Way>("U1.Vdd");
+    auto pU1Vss = std::make_shared<Terminal_2Way>("U1.Vss");
 
 
     std::list<std::shared_ptr<Terminal_2Way>> passives{pZ1, pZ2, pZ3, pZ4};
-    std::list<std::shared_ptr<Terminal_2Way>> sources{pVin};
-    std::list<std::shared_ptr<Terminal_2Way>> components{pU1Out, pU1P, pU1N};
+    std::list<std::shared_ptr<Terminal_2Way>> sources{pVin, pVdd};
+    std::list<std::shared_ptr<Terminal_2Way>> components{pU1Out, pU1P, pU1N, pU1Vdd, pU1Vss};
+
+    std::list<std::shared_ptr<Terminal_2Way>> design;
+    design.insert(design.end(), passives.begin(), passives.end());
+    design.insert(design.end(), sources.begin(), sources.end());
+    design.insert(design.end(), components.begin(), components.end());
+
+
 
     /*
     Z1
@@ -35,8 +57,8 @@ int main(){
         R: Z3-L, Z2-L
     */
     pZ1->pLNode->connect(pVin->pRNode);
-    pZ1->pRNode->connect(pZ3->pLNode);
     pZ1->pRNode->connect(pZ2->pLNode);
+    pZ1->pRNode->connect(pZ3->pLNode);
 
 
     /*
@@ -44,24 +66,28 @@ int main(){
         L: Z1-R, Z3-L
         R: Z4-L, U1.P-L
     */
-    pZ2->pLNode->connect(pZ1->pRNode);
+    // pZ2->pLNode->connect(pZ1->pRNode);
     pZ2->pLNode->connect(pZ3->pLNode);
 
     pZ2->pRNode->connect(pZ4->pLNode);
     pZ2->pRNode->connect(pU1P->pLNode);
-   
-    // z2.rNode.connect(z4.lNode);
-    // z2.rNode.connect(u1P.lNode);
 
     /*
     Z3
         L: Z1-R, Z2-L
         R: U1.Out-R, U1.N-L
     */
-    // z3.lNode.connect(z1.rNode);
-    // z3.lNode.connect(z2.lNode);
-    // z3.rNode.connect(u1Out.rNode);
-    // z3.rNode.connect(u1N.lNode);
+    // pZ3->pLNode->connect(pZ1->pRNode);
+    // pZ3->pLNode->connect(pZ2->pLNode);
+
+    pZ3->pRNode->connect(pU1Out->pRNode);
+    pZ3->pRNode->connect(pU1N->pLNode);
+
+    pZ4->pRNode->connect(pVin->pLNode);
+    pZ4->pRNode->connect(pVdd->pLNode);
+    pZ4->pRNode->connect(pU1Vss->pLNode);
+
+    pU1Vdd->pLNode->connect(pVdd->pRNode);
 
     netlist::NodeList pbNodeList;
     netlist::Node* pbNode;
@@ -69,7 +95,7 @@ int main(){
     std::cout << "List:" << std::endl;
     std::list<std::shared_ptr<Terminal_2Way>>::iterator itr;
     std::unordered_set<std::shared_ptr<Node>>::iterator itrLinks;
-    for (itr = passives.begin(); itr != passives.end(); itr++){
+    for (itr = design.begin(); itr != design.end(); itr++){
         
 
         std::cout << (*itr)->name << std::endl;
--- protobuf/python/dump_msg.py
+++ protobuf/python/dump_msg.py
@@ -1,5 +1,6 @@
 
 import sys
+import code
 
 # Plz don't @ me - this isn't a final way of handling the import :)
 sys.path.append("../../cppsim/build/host")
@@ -9,4 +10,12 @@ nodelist = netlist_pb2.NodeList()
 with open("../../pb_netlist.bin", "rb") as f:
     nodelist.ParseFromString(f.read())
 
-print(nodelist.nodes)
+# print(nodelist.nodes)
+
+
+for node in nodelist.nodes:
+    print(f"{node.name}: {[_.name for _ in node.connections]}")
+          
+
+# code.interact(local=locals())
+
